80ca981739668e76e5a7e9beb8d43afa1fdf88a7
Miromanov Maxim <58211981+Stoppery@users.noreply.github.com>
2023-12-05 13:31:01 +0300

    Update Form.js for input bug

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,16 +10,26 @@ function Form({formDataToParent}) {
 	const [ipData, setIpData] = useState('');
 
 	async function sendData(event) {
-		event.preventDefault();
-		const API_KEY = process.env.REACT_APP_API_KEY;
-		const link = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + API_KEY;
-		let ipAdress = '&ipAddress=' + ipData;
 
-		let response = await fetch(link+ipAdress);
-		let tmpResult = await response.json();
+		let inputEl = document.getElementsByClassName('input-ip')[0];
 
-		
-		formDataToParent(tmpResult);
+		if(ipData && ipData.match(/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/g)){
+			event.preventDefault();
+			inputEl.style.border = '0';
+			const API_KEY = process.env.REACT_APP_API_KEY;
+			const link = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + API_KEY;
+			let ipAdress = '&ipAddress=' + ipData;
+	
+			let response = await fetch(link+ipAdress);
+			let tmpResult = await response.json();
+	
+			
+			formDataToParent(tmpResult);
+		}
+		else {
+			event.preventDefault();
+			inputEl.style.border = '1px solid red';
+		}
 	}
 
 	return (
